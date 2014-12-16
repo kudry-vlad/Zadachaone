b7bfca07302919e7b603cd43afa50197e8fa1740
kudry-vlad <kudry-vlad523@mail.ru>
2014-12-16 05:58:37 +0300

    Update bolshe.cpp

--- bolshe.cpp
+++ bolshe.cpp
@@ -8,26 +8,38 @@ int main(void)
 	setlocale(LC_ALL, "Russian");
 	cout << "Решаем уравнение ax^2+bx+c=0, введите a, b, c: ";
 	cin >> a >> b >> c;
-	if (a == 0.0)
+	if (b == 0.0)
 	{
-		x = -c / b;
-			cout << "x=" << x << endl;
-	}
+		x = sqrt(-c / a);
+		{
+			if (x < 0)
+				cout << "Корней нет" << endl;
+			else
+				cout << "x=" << x << endl;
+	     }
+		}
 	else
 	{
-
-		double x1, x2, d;
-		d = b*b - 4 * a * c;
-		if (d < 0.0)
-			cout << "Корней нет" << endl;
-		else if (d == 0.0)
-			cout << "x=" << -b / (2 * a) << endl;
+		if (a == 0.0)
+		{
+			x = -c / b;
+			cout << "x=" << x << endl;
+		}
 		else
 		{
-			d = sqrt(d);
-			x1 = (-b + d) / (2 * a);
-			x2 = (-b - d) / (2 * a);
-			cout << "x1=" << x1 << " x2=" << x2 << endl;
+			double x1, x2, d;
+			d = b*b - 4 * a * c;
+			if (d < 0.0) 
+				cout << "Корней нет" << endl;
+			else if (d == 0.0)
+				cout << "x=" << -b / (2 * a) << endl;
+			else
+			{
+				d = sqrt(d);
+				x1 = (-b + d) / (2 * a);
+				x2 = (-b - d) / (2 * a);
+				cout << "x1=" << x1 << " x2=" << x2 << endl;
+			}
 		}
 	}
 		return 0;
